fd3b78d18986cea85b136d239c9bb723cac9f1c8
ansh <agansh06@gmail.com>
2025-08-11 21:00:14 +0530

    updated

---
+++ src/Methods2/BMI.java
@@ -0,0 +1,18 @@
+//An organization took up the exercise to find the Body Mass Index (BMI) of all the persons in the team of 10 members.
+// For this create a program to find the BMI and display the height, weight, BMI and status of each individual
+//Hint =>
+//Take user input in double for the weight (in kg) of the person and height (in cm) for the person and
+// store it in the corresponding 2D array of 10 rows and 3 columns. The First Column storing the weight,
+// the second column storing the height in cm and the third column is the BMI
+//Create a Method to find the BMI of every person and populate the array.
+// Use the formula BMI = weight / (height * height). Note unit is kg/m^2. For this convert cm to meter
+//Create a Method to determine the BMI status using the logic shown in the figure below.
+// and return the array of all the persons BMI Status.
+package Methods2;
+import java.util.Scanner;
+public class BMI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
